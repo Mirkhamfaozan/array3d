dc84139120ad311dab07c296c9b872ba63a82e14
aress.mhmd <100754810+Mirkhamfaozan@users.noreply.github.com>
2024-07-24 23:45:26 +0700

    Add files via upload

---
+++ Array3D/src/array3d/Array3D.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package array3d;
+
+/**
+ *
+ * @author IRKHAM
+ */
+public class Array3D {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int [][][] data = {// TODO code application logic here
+            {
+                {1,2},
+                {3,4}
+                
+            },
+                    
+    
+                   
+            {
+                {5,6},
+                {7,8},
+                {9,10}
+            }
+    };
+    
+    System.out.println(data[1][1][1]);
+    }
+}
